d78d0545819e83b0a85bca5cc7df47152c9fb528
@kavindi.tharushika98 <kavindi.tharushika98@gmail.com>
2022-12-27 13:45:04 +0530

    update register-2

--- app/views/clinicattendees/register.php
+++ app/views/clinicattendees/register.php
@@ -147,6 +147,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -155,13 +156,14 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
     <title><?php echo SITENAME; ?></title>
 </head>
+
 <body>
     <?php require APPROOT . '/views/inc/navbar.php' ; ?>
     <?php require APPROOT . '/views/inc/sidebar.php' ; ?>
 
 
     <div class="container_new1">
-        <br> <br> <br> <br> 
+        <br> <br> <br> <br>
         <h2>R E G I S T R A T I O N</h2>
         <br>
         <!-- <p>Please enter your details to register</p> > -->
@@ -169,126 +171,127 @@
 
         <div class="makeflex">
 
-        
-        <form action="<?php echo URLROOT; ?>/clinicattendees/register" method="post">
-            <div class="register_CA">
-                <h4>Mother's details</h4>
 
-                <br>
-                <!--"class="user-details"-->
-                <div class="input-box">
-                    <label for="mname">Name: <sup>*</sup></label>
-                    <input type="text" name="mname" placeholder="Enter your name here...">
-                    <span class="form-err"><?php echo $data['mname_err']; ?></span>
-                </div>
-                <div class="input-box">
-                    <label for="nic">ID No: <sup>*</sup></label>
-                    <input type="text" maxlength="12" name="nic" placeholder="Enter your ID no here...">
-                    <span class="form-err"><?php echo $data['nic_err']; ?></span>
-                </div>
-                <div class="input-box">
-                    <label for="mage">Age: <sup>*</sup></label>
-                    <input type="text" maxlength="2"  name="mage" placeholder="Enter your age here...">
-                    <span class="form-err"><?php echo $data['mage_err']; ?></span>
-                </div>
-                <div class="input-box">
-                    <label for="gravidity">Gravidity: <sup>*</sup></label>
-                    <input type="text" maxlength="2"  name="gravidity" placeholder="Enter your no of pregnancies here...">
-                    <span class="form-err"><?php echo $data['gravidity_err']; ?></span>
-                </div>
-                <div class="input-box">
-                    <label for="mlevelofeducation">Level of Education: <sup>*</sup></label>
-                    <select name="mlevelofeducation" id="mlevelofeducation">
-                        <option value="">Select Your level of Eduction</option>
-                        <option value="O/L">O/L</option>
-                        <option value="A/L">A/L</option>
-                        <option value="Higher education">Higher education</option>
-                    </select>
+            <form action="<?php echo URLROOT; ?>/clinicattendees/register" method="post">
+                <div class="register_CA">
+                    <h4>Mother's details</h4>
 
-                    <span class="form-err"><?php echo $data['mlevelofeducation_err']; ?></span>
-                </div>
-                <div class="input-box">
-                    <label for="moccupation">Occupation: <sup>*</sup></label>
-                    <input type="text" name="moccupation" placeholder="Enter your occupation here...">
-                    <span class="form-err"><?php echo $data['moccupation_err']; ?></span>
-                </div>
-                <div class="input-box">
-                    <label for="mcontactno">Contact No: <sup>*</sup></label>
-                    <input type="text" name="mcontactno" placeholder="Enter your contact no here...">
-                    <span class="form-err"><?php echo $data['mcontactno_err']; ?></span>
-                </div>
-                <div class="input-box">
-                    <label for="address">Address: <sup>*</sup></label>
-                    <input type="text" maxlength="10"  name="address" placeholder="Enter your address here...">
-                    <span class="form-err"><?php echo $data['address_err']; ?></span>
-                </div>
-                <div class="input-box">
-                    <label for="memail">E-mail: <sup>*</sup></label>
-                    <input type="email" name="memail" placeholder="Enter your email here...">
-                    <span class="form-err"><?php echo $data['memail_err']; ?></span>
-                </div>
+                    <br>
+                    <!--"class="user-details"-->
+                    <div class="input-box">
+                        <label for="mname">Name: <sup>*</sup></label>
+                        <input type="text" name="mname" placeholder="Enter your name here...">
+                        <span class="form-err"><?php echo $data['mname_err']; ?></span>
+                    </div>
+                    <div class="input-box">
+                        <label for="nic">NIC: <sup>*</sup></label>
+                        <input type="text" maxlength="12" name="nic" placeholder="Enter your ID no here...">
+                        <span class="form-err"><?php echo $data['nic_err']; ?></span>
+                    </div>
+                    <div class="input-box">
+                        <label for="mage">Age: <sup>*</sup></label>
+                        <input type="text" maxlength="2" name="mage" placeholder="Enter your age here...">
+                        <span class="form-err"><?php echo $data['mage_err']; ?></span>
+                    </div>
+                    <div class="input-box">
+                        <label for="gravidity">Gravidity: <sup>*</sup></label>
+                        <input type="text" maxlength="2" name="gravidity"
+                            placeholder="Enter your no of pregnancies here...">
+                        <span class="form-err"><?php echo $data['gravidity_err']; ?></span>
+                    </div>
+                    <div class="input-box">
+                        <label for="mlevelofeducation">Level of Education: <sup>*</sup></label>
+                        <select name="mlevelofeducation" id="mlevelofeducation">
+                            <option value="">Select Your level of Eduction</option>
+                            <option value="O/L">O/L</option>
+                            <option value="A/L">A/L</option>
+                            <option value="Higher education">Higher education</option>
+                        </select>
 
-            </div>
+                        <span class="form-err"><?php echo $data['mlevelofeducation_err']; ?></span>
+                    </div>
+                    <div class="input-box">
+                        <label for="moccupation">Occupation: <sup>*</sup></label>
+                        <input type="text" name="moccupation" placeholder="Enter your occupation here...">
+                        <span class="form-err"><?php echo $data['moccupation_err']; ?></span>
+                    </div>
+                    <div class="input-box">
+                        <label for="mcontactno">Contact No: <sup>*</sup></label>
+                        <input type="text" name="mcontactno" placeholder="Enter your contact no here...">
+                        <span class="form-err"><?php echo $data['mcontactno_err']; ?></span>
+                    </div>
+                    <div class="input-box">
+                        <label for="address">Address: <sup>*</sup></label>
+                        <input type="text" maxlength="10" name="address" placeholder="Enter your address here...">
+                        <span class="form-err"><?php echo $data['address_err']; ?></span>
+                    </div>
+                    <div class="input-box">
+                        <label for="memail">E-mail: <sup>*</sup></label>
+                        <input type="email" name="memail" placeholder="Enter your email here...">
+                        <span class="form-err"><?php echo $data['memail_err']; ?></span>
+                    </div>
 
-
-
-            <div class="register_CA">
-                <h4>Father's details</h4>
-                <br>
-                <div class="input-box">
-                    <label for="hname">Name: </label>
-                    <input type="text" name="hname" placeholder="Enter name here...">
-                    <!-- <span class="form-err"><!?php echo $data['hname_err']; ?></span> -->
-                </div>
-                <div class="input-box">
-                    <label for="hage">Age: </label>
-                    <input type="text" maxlength="2"  name="hage" placeholder="Enter age here...">
-                    <!-- <span class="form-err"><!?php echo $data['hage_err']; ?></span> -->
-                </div>
-                <div class="input-box">
-                    <label for="hlevelofeducation">Level of Education: </label>
-                    <select name="hlevelofeducation" id="hlevelofeducation">
-                        <option value="">Select Your level of Eduction</option>
-                        <option value="O/L">O/L</option>
-                        <option value="A/L">A/L</option>
-                        <option value="Higher education">Higher education</option>
-                    </select>
-                    <!-- <span class="form-err"><!?php echo $data['hlevelofeducation_err']; ?></span> -->
-                </div>
-                <div class="input-box">
-                    <label for="hoccupation">Occupation: </label>
-                    <input type="text" name="hoccupation" placeholder="Enter occupation here...">
-                    <!-- <span class="form-err"><!?php echo $data['hoccupation_err']; ?></span> -->
-                </div>
-                <div class="input-box">
-                    <label for="hcontactno">Contact no: </label>
-                    <input type="text" maxlength="10"  name="hcontactno" placeholder="Enter contact no here...">
-                    <!-- <span class="form-err"><!?php echo $data['hcontactno_err']; ?></span> -->
-                </div>
-                <div class="input-box">
-                    <label for="hemail">E-mail: </label>
-                    <input type="email" name="hemail" placeholder="Enter e-mail here...">
-                    <!-- <span class="form-err"><!?php echo $data['hemail_err']; ?></span> -->
                 </div>
 
 
-                <br>
 
-                <h4>Other details</h4>
-                <br>
+                <div class="register_CA">
+                    <h4>Father's details</h4>
+                    <br>
+                    <div class="input-box">
+                        <label for="hname">Name: </label>
+                        <input type="text" name="hname" placeholder="Enter name here...">
+                        <!-- <span class="form-err"><!?php echo $data['hname_err']; ?></span> -->
+                    </div>
+                    <div class="input-box">
+                        <label for="hage">Age: </label>
+                        <input type="text" maxlength="2" name="hage" placeholder="Enter age here...">
+                        <!-- <span class="form-err"><!?php echo $data['hage_err']; ?></span> -->
+                    </div>
+                    <div class="input-box">
+                        <label for="hlevelofeducation">Level of Education: </label>
+                        <select name="hlevelofeducation" id="hlevelofeducation">
+                            <option value="">Select Your level of Eduction</option>
+                            <option value="O/L">O/L</option>
+                            <option value="A/L">A/L</option>
+                            <option value="Higher education">Higher education</option>
+                        </select>
+                        <!-- <span class="form-err"><!?php echo $data['hlevelofeducation_err']; ?></span> -->
+                    </div>
+                    <div class="input-box">
+                        <label for="hoccupation">Occupation: </label>
+                        <input type="text" name="hoccupation" placeholder="Enter occupation here...">
+                        <!-- <span class="form-err"><!?php echo $data['hoccupation_err']; ?></span> -->
+                    </div>
+                    <div class="input-box">
+                        <label for="hcontactno">Contact no: </label>
+                        <input type="text" maxlength="10" name="hcontactno" placeholder="Enter contact no here...">
+                        <!-- <span class="form-err"><!?php echo $data['hcontactno_err']; ?></span> -->
+                    </div>
+                    <div class="input-box">
+                        <label for="hemail">E-mail: </label>
+                        <input type="email" name="hemail" placeholder="Enter e-mail here...">
+                        <!-- <span class="form-err"><!?php echo $data['hemail_err']; ?></span> -->
+                    </div>
 
-                <div class="input-box">
-                    <label for="gnd">Grama Niladhari Division: <sup>*</sup></label>
-                    <select name="gnd" id="gnd">
-                        <option value="" selected hidden>Grama Niladhari division</option>
-                        <option value="GND-1">GND-1</option>
-                        <option value="GND-2">GND-2</option>
-                        <option value="GND-3">GND-3</option>
-                        <option value="GND-4">GND-4</option>
-                    </select>
-                    <span class="form-err"><?php echo $data['gnd_err']; ?></span>
-                </div>
-                <!-- <div class="input-box">
+
+                    <br>
+
+                    <h4>Other details</h4>
+                    <br>
+
+                    <div class="input-box">
+                        <label for="gnd">Grama Niladhari Division: <sup>*</sup></label>
+                        <select name="gnd" id="gnd">
+                            <option value="" selected hidden>Grama Niladhari division</option>
+                            <option value="GND-1">GND-1</option>
+                            <option value="GND-2">GND-2</option>
+                            <option value="GND-3">GND-3</option>
+                            <option value="GND-4">GND-4</option>
+                        </select>
+                        <span class="form-err"><?php echo $data['gnd_err']; ?></span>
+                    </div>
+                    <!-- <div class="input-box">
                         <label for="phm">PHM Area: <sup>*</sup></label>
                         <select name="phm" id="phm">
                             <option value="">Select Public Health Midwife Area</option>
@@ -299,21 +302,21 @@
                         </select>
                         <span class="form-err"></?php echo $data['phm_err']; ?></span>
                     </div> -->
-                <!-- <div class="input-box">
+                    <!-- <div class="input-box">
                         <label for="password">Password: <sup>*</sup></label>
                         <input type="text" name="password" placeholder="Enter password here...">
                         <span class="form-err"></?php echo $data['password_err']; ?></span>
                     </div> -->
 
                     <br>
-                <div class="input-box">
-                    <input type="submit" value="Submit">
-                    <br> <br> <br>
+                    <div class="input-box">
+                        <input type="submit" value="Submit">
+                        <br> <br> <br>
+                    </div>
+
                 </div>
 
-            </div>
-
-        </form>
+            </form>
         </div>
 
     </div>
